a588e4491bcfe9b3722b2b32c95de4c04e3b414c
Theo Andrup <theo.andrup@t-online.de>
2025-05-31 15:16:40 +0200

    move

--- src/db.js
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-import knexConfig from '../knexfile.js';
-
-const environment = process.env.NODE_ENV || 'development';
-const db = knex(knexConfig[environment]);
-
-export { db };
-export default db;
